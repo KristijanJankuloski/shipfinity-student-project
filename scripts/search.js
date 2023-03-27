520a0d61a659ac5b2b135bed4593258ddb174f4a
Kristijan <jankuloski.k@gmail.com>
2023-03-27 21:50:37 +0200

    Added documentation folder

--- scripts/search.js
+++ scripts/search.js
@@ -3,3 +3,8 @@ import headerContainer from "../components/header.js";
 import footerContainer from "../components/footer.js";
 document.getElementById("header").appendChild(headerContainer);
 document.getElementById("footer").appendChild(footerContainer);
+
+// Keep this on top
+// The search string of the user is available in the keyword constant you see here
+const searchString = new URLSearchParams(window.location.search);
+const keyword = searchString.get('search');
