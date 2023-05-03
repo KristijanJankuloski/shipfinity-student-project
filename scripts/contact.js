b8439c6a93e21b8f76c0ed71b70bbfc87d109589
Mihail2802 <mihailkuzmanovski@live.com>
2023-05-03 22:25:03 +0200

    slight change

--- scripts/contact.js
+++ scripts/contact.js
@@ -4,3 +4,18 @@ import footerContainer from "../components/footer.js";
 document.getElementById("header").appendChild(headerContainer);
 document.getElementById("footer").appendChild(footerContainer);
 
+let firstNameField = document.getElementById("contactFirstNameField");
+let lastNameField = document.getElementById("contactLastNameField");
+let eMailField = document.getElementById("contactEMailField");
+let messageField = document.getElementById("contactSubjectField");
+
+console.log(firstNameField)
+
+let contactSubmitBtn = document.getElementById("exampleModal");
+
+contactSubmitBtn.addEventListener("click", function () {
+  firstNameField.innerText = "";
+  lastNameField.innerHTML = "";
+  eMailField.innerHTML = "";
+  messageField.innerHTML= "";
+});
