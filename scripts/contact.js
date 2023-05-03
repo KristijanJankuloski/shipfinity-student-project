3025d7e74a61943d8e37fb7755127c90eb89fcb3
Mihail2802 <mihailkuzmanovski@live.com>
2023-05-03 15:40:33 +0200

    contact page html & css done

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,3 +3,4 @@ import headerContainer from "../components/header.js";
 import footerContainer from "../components/footer.js";
 document.getElementById("header").appendChild(headerContainer);
 document.getElementById("footer").appendChild(footerContainer);
+
