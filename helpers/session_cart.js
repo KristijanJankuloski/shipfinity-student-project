a1f9e6fc615b5443c8c1b47437cfca0196b6b95f
kristijan <jankuloski.k@gmail.com>
2023-05-04 13:54:26 +0200

    added session helpers

--- helpers/session_cart.js
+++ helpers/session_cart.js
@@ -17,4 +17,24 @@ function setCart(newCart){
     sessionStorage.setItem('shoppingCart', convertedCart);
 }
 
-export { getCart, setCart };
+function addToCart(product){
+    let cartInStorage = sessionStorage.getItem('shoppingCart');
+    if(cartInStorage){
+        shoppingCart = JSON.parse(cartInStorage);
+    }
+    shoppingCart.push(product);
+    let cartString = JSON.stringify(shoppingCart);
+    sessionStorage.setItem('shoppingCart', cartString);
+}
+
+function deleteById(id){
+    let cartInStorage = sessionStorage.getItem('shoppingCart');
+    if(cartInStorage){
+        shoppingCart = JSON.parse(cartInStorage);
+    }
+    shoppingCart.splice(shoppingCart.findIndex(prod => prod.id === id), 1);
+    let cartString = JSON.stringify(shoppingCart);
+    sessionStorage.setItem('shoppingCart', cartString);
+}
+
+export { getCart, setCart, addToCart, deleteById };
