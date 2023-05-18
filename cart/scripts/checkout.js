0b501e49d3ccb95e5775238c7f4ccead43e5e840
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-05-18 18:55:56 +0200

    Small changes in checkout.html and checkout.js

--- cart/scripts/checkout.js
+++ cart/scripts/checkout.js
@@ -1,5 +1,13 @@
 import headerContainer from '../../components/header.js';
 import footerContainer from '../../components/footer.js';
+import { getCart } from '../../helpers/session_cart.js';
 
 document.getElementById("header").appendChild(headerContainer);
 document.getElementById("footer").appendChild(footerContainer);
+
+// Will change this function. Just a test
+function printMessageAfterCheckout() {
+    alert("Order received successfully");
+}
+
+printMessageAfterCheckout();
