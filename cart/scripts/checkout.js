e5a683a67276d94c09f680c42daf2530d4589c89
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-06-24 15:18:37 +0200

    Slight change in checkout page.

--- cart/scripts/checkout.js
+++ cart/scripts/checkout.js
@@ -5,9 +5,18 @@ import { getCart } from '../../helpers/session_cart.js';
 document.getElementById("header").appendChild(headerContainer);
 document.getElementById("footer").appendChild(footerContainer);
 
-// Will change this function. Just a test
-function printMessageAfterCheckout() {
-    alert("Order received successfully");
-}
+let checkoutBtn = document.querySelector(".btn-submitCheckout");
 
-printMessageAfterCheckout();
+checkoutBtn.addEventListener("click", function() {
+    let userFullName = document.querySelector("#fname").value = "";
+    let userEMail = document.querySelector("#email").value = "";
+    let userAddress = document.querySelector("#address").value = "";
+    let userCity = document.querySelector("#city").value = "";
+    let userZipCode = document.querySelector("#zip").value = "";
+    let userState = document.querySelector("#state").value = "";
+    let userCardName = document.querySelector("#cardName").value = "";
+    let userCreditCardNumber = document.querySelector("#creditCardNumber").value = "";
+    let userCreditCardExpiryMonth = document.querySelector("#expiryMonth").value = "";
+    let userCardExpiryYear = document.querySelector("#expiryYear").value = "";
+    let userCreditCardCVV = document.querySelector("#cvv").value = "";
+});
